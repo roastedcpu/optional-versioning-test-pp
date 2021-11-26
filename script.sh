c906a97a8ceb9037c5440c3abdd0dd7f17f775e3
J. Diogo Oliveira <jdso@vhelsi.com>
2021-11-26 12:43:18 +0000

    ...

--- script.sh
+++ script.sh
@@ -9,11 +9,11 @@ LATEST_TAG_NAME=$(git tag --sort=committerdate | tail -1)
 
 if [ "$CURRENT_COMMIT_HASH" = "$LATEST_TAG_HASH" ]; then
     echo "There's already a tag assigned to HEAD commit. Setting version to $LATEST_TAG_NAME" # use this as version
-    npm version --no-git-tag-version $LATEST_TAG_NAME
+    npm version --no-git-tag-version $LATEST_TAG_NAME || true
 else
     echo "Getting latest tag: $LATEST_TAG_NAME"
     echo "Incrementing minor"
-    npm version --no-git-tag-version $LATEST_TAG_NAME
+    npm version --no-git-tag-version $LATEST_TAG_NAME || true
     yarn version --patch
     VERSION=$(npx project-version)
     git tag -a $VERSION -m "Automatic tagging by pipeline. To set a custom version please tag the last <main> commit before running the pipeline." 
